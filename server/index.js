b2839f5517e3f8f9be5d8c637df9e279f017ca58
Ritvij Arunanjana <ritvij981@gmail.com>
2023-01-12 23:49:35 +0530

    gcp

--- server/index.js
+++ server/index.js
@@ -11,14 +11,14 @@ const orderRoute = require("./routes/order")
 const stripeRoute = require("./routes/stripe");
 const cors = require("cors")
 
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("DBConnection Successful!");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+  mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+      console.log("DBConnection Successful!");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 
 app.use(cors())
 app.use(express.json());
